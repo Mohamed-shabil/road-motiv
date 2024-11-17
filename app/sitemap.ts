7519ff1c8aef5523862e9ddfe1c0276fb924169c
mshabil <mohamedshabil231@gmail.com>
2024-11-17 12:07:02 +0530

    completely done

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -10,13 +10,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
             priority: 1,
         },
         {
-            url: `${baseUrl}about`,
+            url: `${baseUrl}/about`,
             lastModified: new Date(),
             changeFrequency: "monthly",
             priority: 0.8,
         },
         {
-            url: `${baseUrl}contact`,
+            url: `${baseUrl}/contact`,
             lastModified: new Date(),
             changeFrequency: "monthly",
             priority: 0.5,
